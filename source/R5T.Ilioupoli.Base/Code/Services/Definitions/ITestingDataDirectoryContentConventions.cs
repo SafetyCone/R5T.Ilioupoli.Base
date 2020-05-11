0f7ab439e9d2b55ff8c7112cff913917ca37797c
MinexAutomation <minex.automation@gmail.com>
2020-05-10 23:40:52 -0700

    Added example Visual Studio project file paths.

--- source/R5T.Ilioupoli.Base/Code/Services/Definitions/ITestingDataDirectoryContentConventions.cs
+++ source/R5T.Ilioupoli.Base/Code/Services/Definitions/ITestingDataDirectoryContentConventions.cs
@@ -7,8 +7,13 @@ namespace R5T.Ilioupoli
     {
         string BasicTextFileName { get; }
 
+        // Visual Studio solution files.
         string NewVisualStudio2017SolutionFileName { get; }
         string NewVisualStudio2019SolutionFileName { get; }
         string ExampleVisualStudioSolutionFileName { get; }
+
+        // Visual Studio project files.
+        string ExampleVisualStudioProjectFileName01 { get; }
+        string ExampleVisualStudioProjectFileName02 { get; }
     }
 }
