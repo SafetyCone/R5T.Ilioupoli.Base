78c1420904218acb0cfcafbf3203a0b17528fbdc
davidcoats <david@getsafetycone.com>
2022-04-27 15:31:51 -0700

    Interim changes.

--- source/R5T.Ilioupoli.Base/Code/Services/Definitions/ITestingDataDirectoryContentConventions.cs
+++ source/R5T.Ilioupoli.Base/Code/Services/Definitions/ITestingDataDirectoryContentConventions.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Ilioupoli
-{
-    public interface ITestingDataDirectoryContentConventions
+{[ServiceDefinitionMarker]
+    public interface ITestingDataDirectoryContentConventions:IServiceDefinition
     {
         string BasicTextFileName { get; }
 
